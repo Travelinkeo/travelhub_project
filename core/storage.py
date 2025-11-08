bf7723c5cacfe64792f0e8d91969f2798deb1753
Travelinkeo <travelinkeo@gmail.com>
2025-11-08 19:01:49 -0400

    Fix: Eliminar prefijo media/ en Cloudinary URLs

--- core/storage.py
+++ core/storage.py
@@ -4,8 +4,6 @@ from cloudinary_storage.storage import RawMediaCloudinaryStorage
 class RawFileStorage(RawMediaCloudinaryStorage):
     """Storage para archivos raw (PDF, TXT, EML) en Cloudinary"""
     
-    def url(self, name):
-        """Generar URL pública para archivos raw"""
-        if not name:
-            return ''
-        return super().url(name)
+    def _prepend_prefix(self, name):
+        """No agregar prefijo 'media/' - usar path directo"""
+        return name

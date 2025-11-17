7293ccf4f2cb1356d08bb262d02456d2d2c925ee
Travelinkeo <travelinkeo@gmail.com>
2025-11-17 07:42:05 -0400

    feat(frontend): convert to progressive web app (pwa)

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,3 +1,5 @@
+import withPWAInit from "@ducanh2912/next-pwa";
+
 /** @type {import('next').NextConfig} */
 const securityHeaders = [
   {
@@ -34,6 +36,13 @@ const securityHeaders = [
     ].join('; ')
   }
 ];
+
+const withPWA = withPWAInit({
+  dest: "public",
+  disable: process.env.NODE_ENV === "development",
+  register: true,
+  skipWaiting: true,
+});
 
 const nextConfig = {
   reactStrictMode: true,
@@ -80,4 +89,4 @@ const nextConfig = {
   },
 };
 
-export default nextConfig;
+export default withPWA(nextConfig);

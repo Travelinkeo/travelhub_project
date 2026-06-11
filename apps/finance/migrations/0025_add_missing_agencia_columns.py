82b5de687fbec13c1da2cec587be5a2add42d8b2
Armando TravelHub <armando@travelhub.com>
2026-06-11 07:23:22 -0400

    Wrap conflicting database operations in SeparateDatabaseAndState for finance and marketing migrations

--- apps/finance/migrations/0025_add_missing_agencia_columns.py
+++ apps/finance/migrations/0025_add_missing_agencia_columns.py
@@ -9,26 +9,31 @@ class Migration(migrations.Migration):
     ]
 
     operations = [
-        migrations.AddField(
-            model_name="linkdepago",
-            name="agencia",
-            field=models.ForeignKey(
-                blank=True,
-                null=True,
-                on_delete=django.db.models.deletion.CASCADE,
-                related_name="linkdepago_items",
-                to="core.agencia",
-            ),
-        ),
-        migrations.AddField(
-            model_name="retencionislr",
-            name="agencia",
-            field=models.ForeignKey(
-                blank=True,
-                null=True,
-                on_delete=django.db.models.deletion.CASCADE,
-                related_name="retencionislr_items",
-                to="core.agencia",
-            ),
+        migrations.SeparateDatabaseAndState(
+            database_operations=[],
+            state_operations=[
+                migrations.AddField(
+                    model_name="linkdepago",
+                    name="agencia",
+                    field=models.ForeignKey(
+                        blank=True,
+                        null=True,
+                        on_delete=django.db.models.deletion.CASCADE,
+                        related_name="linkdepago_items",
+                        to="core.agencia",
+                    ),
+                ),
+                migrations.AddField(
+                    model_name="retencionislr",
+                    name="agencia",
+                    field=models.ForeignKey(
+                        blank=True,
+                        null=True,
+                        on_delete=django.db.models.deletion.CASCADE,
+                        related_name="retencionislr_items",
+                        to="core.agencia",
+                    ),
+                ),
+            ],
         ),
     ]
